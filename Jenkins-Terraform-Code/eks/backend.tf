2d98bb64ec91eb07374b4412b58755b3b7d0f064
Priya Salunkhe <77096978+Kaustubh-tech17@users.noreply.github.com>
2025-11-19 19:30:31 +0530

    Update backend.tf

--- Jenkins-Terraform-Code/eks/backend.tf
+++ Jenkins-Terraform-Code/eks/backend.tf
@@ -8,8 +8,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-kaviarasu-tf-bucket"
-    region         = "us-east-1"
+    bucket         = "dev-kaviarasu-tf-bucket1"
+    region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
